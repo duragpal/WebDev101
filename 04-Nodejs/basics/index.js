a5b4182171824f4a00cfdf709bc5a49a8a755aab
Durag Pal Singh <107455742+duragpal@users.noreply.github.com>
2024-09-15 19:46:42 +0530

    added nodejs

---
+++ 04-Nodejs/basics/index.js
@@ -0,0 +1,6 @@
+const chalk = require("chalk");
+let name = "Durag";
+console.log(chalk.blue(name));
+
+//Run code : node index.js
+//Or add in script: --- "start": "node index.js" --run via..npm run start
